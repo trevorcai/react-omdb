567d02aefafe889871103979cbb821693e206e56
Trevor Cai <trevorcai@users.noreply.github.com>
2016-09-16 13:41:01 -0700

    Quick-n-dirty localStorage

--- ui/setup.jsx
+++ ui/setup.jsx
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
+import Immutable from 'immutable';
 import reducer from './reducer';
 import { watchForSearch, watchForLoadSingle } from './sagas';
 
+let store;
 window.React = React;
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const persistedState = localStorage.getItem('reduxState');
+if (persistedState) {
+  const parsedState = JSON.parse(persistedState);
+  if (parsedState.movies) {
+    parsedState.movies = new Immutable.List(parsedState.movies);
+  }
+  const immutableState = new Immutable.Map(parsedState);
+
+  store = createStore(reducer, immutableState, applyMiddleware(sagaMiddleware));
+} else {
+  store = createStore(reducer, applyMiddleware(sagaMiddleware));
+}
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
 sagaMiddleware.run(watchForSearch);
 sagaMiddleware.run(watchForLoadSingle);
 
